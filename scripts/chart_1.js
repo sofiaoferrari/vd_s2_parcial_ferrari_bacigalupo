eff593c767077f5549ad06ca1f5053be4d26cd26
guillebaci <guillerminabacigalupo@hotmail.com>
2023-04-18 15:04:45 -0300

    agrego el mapa del chart1 y el archivo de barrios geojson

--- scripts/chart_1.js
+++ scripts/chart_1.js
@@ -1,19 +1,57 @@
-d3.csv('data/147_vehiculos_mal_estacionados.csv', d3.autoType).then(data => {
-  let chart = Plot.plot({
-    width: 600,
-    height: 200,
-    line: true,
-    nice: true,
-    zero: true,
-    grid: true,
+const mapaFetch = d3.json('./data/barrios-caba.geojson')
+const dataFetch = d3.dsv(';', './data/147_vehiculos_mal_estacionados.csv', d3.autoType)
+
+Promise.all([mapaFetch, dataFetch]).then(([barrios, data]) => {
+  
+  /* Agrupamos reclamos x barrio */
+  const reclamosPorBarrio = d3.group(data, d => d.domicilio_barrio) // crea un Map
+  console.log('reclamosPorBarrio', reclamosPorBarrio)
+  
+  /* A cada feature del mapa le agregamos la prop DENUNCIAS */
+  barrios.features.forEach(d => {
+    let nombreBarrio = d.properties.BARRIO
+    let cantReclamos =  reclamosPorBarrio.get(nombreBarrio).length
+    d.properties.DENUNCIAS = cantReclamos
+
+    console.log(nombreBarrio + ': ' + cantReclamos)
+  })
+
+
+  /* Mapa Coroplético */
+  let chartMap = Plot.plot({
+    // https://github.com/observablehq/plot#projection-options
+    projection: {
+      type: 'mercator',
+      domain: barrios, // Objeto GeoJson a encuadrar
+    },
+    color: {
+      // Quantize continuo (cant. denuncias) -> discreto (cant. colores)
+      type: 'quantize', 
+      n: 10,
+      scheme: 'ylorbr',
+      label: 'Cantidad de denuncias',
+      legend: true,
+    },
     marks: [
-      Plot.dot(data, {
-        x: 'fertility',
-        y: 'cluster',
-        fillOpacity: 0.6,
-        symbol: 'square',
+      Plot.geo(barrios, {
+        fill: d => d.properties.DENUNCIAS,
+        stroke: 'gray',
+        title: d => `${d.properties.BARRIO}\n${d.properties.DENUNCIAS} denuncias`,
       }),
+      Plot.text(
+        barrios.features,
+        Plot.centroid({
+          text: (d) => d.properties.BARRIO,
+          fill: "currentColor",
+          stroke: "white",
+          textAnchor: "center",
+          dx: 4,
+          filter: (d) => d.properties.DENUNCIAS > 80
+        })
+      )
     ],
   })
-  d3.select('#chart_1').append(() => chart)
+
+  /* Agregamos al DOM la visualización chartMap */
+  d3.select('#chart_1').append(() => chartMap)
 })
