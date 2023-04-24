d4108eecec83c0b01a55fb68b573ac338fab8deb
Guillermina Bacigalupo <93266820+guillebaci@users.noreply.github.com>
2023-04-24 18:50:13 -0300

    Update chart_2.js

--- scripts/chart_2.js
+++ scripts/chart_2.js
@@ -1,20 +1,20 @@
-d3.csv('data/147_vehiculos_mal_estacionados.csv', d3.autoType).then(data => {
-  let chart = Plot.plot({
-    marks: [
-      Plot.dot(data, {
-        x: 'fertility',
-        y: 'life_expect',
-        stroke: 'cluster',
-        r: 'pop',
-      }),
-    ],
-    grid: true,
-    line: true,
-    nice: true,
-    color: {
-      legend: true,
-    },
-  })
-
-  d3.select('#chart_2').append(() => chart)
-})
+d3.dsv(";","./data/147_vehiculos_mal_estacionados.csv", d3.autoType).then((data) => {
+    let dataPalermo = data.filter(d => d.domicilio_barrio == "PALERMO");
+    let dataAME = data.filter(d => d.prestacion == "VEHÍCULO MAL ESTACIONADO");
+    let chart = Plot.plot({
+      marks: [
+        Plot.dot(dataPalermo, {
+          x: 'hora_ingreso',
+          y: 'domiclio_calle',
+          r: dataAME,
+          fill: 'cluster',
+        }),
+      ],
+      color: {
+        scheme: 'dark2',
+        legend: true,
+      },
+    });
+    d3.select('#chart_2').append(() => chart)
+  });
+  
