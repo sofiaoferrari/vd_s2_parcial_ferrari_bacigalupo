3d5d0ffbc79c5d131702acf8fc54cb205404d3d0
Guillermina Bacigalupo <93266820+guillebaci@users.noreply.github.com>
2023-04-25 16:42:18 -0300

    Update chart_2.js

--- scripts/chart_2.js
+++ scripts/chart_2.js
@@ -1,20 +1,29 @@
 d3.dsv(";","./data/147_vehiculos_mal_estacionados.csv", d3.autoType).then((data) => {
-    let dataPalermo = data.filter(d => d.domicilio_barrio == "PALERMO");
-    let dataAME = data.filter(d => d.prestacion == "VEHÍCULO MAL ESTACIONADO");
-    let chart = Plot.plot({
-      marks: [
-        Plot.dot(dataPalermo, {
-          x: 'hora_ingreso',
-          y: 'domiclio_calle',
-          r: dataAME,
-          fill: 'cluster',
-        }),
-      ],
-      color: {
-        scheme: 'dark2',
-        legend: true,
-      },
-    });
-    d3.select('#chart_2').append(() => chart)
+  let dataPalermo = data.filter(d => d.domicilio_barrio == "PALERMO");
+  let chart = Plot.plot({
+    marks: [
+      Plot.barX(
+        dataPalermo,
+        Plot.groupY({ x: "sum" }, {y: "prestacion"})
+      )
+    ],
+    style: {
+      color: "#B41818",
+      fontSize: "12px",
+    },
+    height: 300,
+    width: 1000,
+    marginLeft: 450,
+    y: { label: "", tickFormat: (d) => formatoEjeX(d)},
   });
-  
+  d3.select("#chart_2").append(() => chart);
+  function formatoEjeX(data){
+    if (data == "INCONVENIENTES PARA EL ESTACIONAMIENTO DE BICICLETAS EN GARAJES"){
+      return "BICICLETAS EN GARAJES"
+    } else if (data == "IRREGULARIDADES CON RESERVAS DE ESPACIOS PARA ESTACIONAMIENTO DE VEHÍCULOS DE PERSONAS CON DISCAPACIDAD") {
+      return "IRREGULARIDADES ESTACIONAMIENTO PERSONAS CON DISCAPACIDAD"
+    } else {
+       return data
+    }
+  }
+})
